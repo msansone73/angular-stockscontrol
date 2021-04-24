9cfc57f48b81ff7dd079656e174f073297d10328
marcio sansone <msansone@gmail.com>
2021-04-24 20:59:09 +0100

    added basic autentication in api request

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
 import { Login } from './../model/login.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -11,24 +11,38 @@ export class LoginService {
 
   baseUrl= environment.baseUrl+"login"
 
-  constructor(private http: HttpClient) { }
+  httpOptions={};
+
+
+  constructor(private http: HttpClient) {
+
+    var headers_object = new HttpHeaders();
+    headers_object.append('Content-Type', 'application/json');
+    headers_object.append("Authorization", "Basic " + btoa("admin:password"));
+
+    this.httpOptions = {
+      headers: headers_object
+    };
+
+   }
 
   
   create(login:Login):Observable<Login>{
-    return this.http.post<Login>(this.baseUrl, login)
+    return this.http.post<Login>(this.baseUrl, login, this.httpOptions)
   }
 
   logar(login:string, password: string): Observable<Login>{
     const loginObs={'login':login, 'password':password}
-    return this.http.post<Login>(this.baseUrl, loginObs)
+    return this.http.post<Login>(this.baseUrl, loginObs, this.httpOptions)
   }
 
   readAll():Observable<Login[]>{
-    return this.http.get<Login[]>(this.baseUrl)
+
+    return this.http.get<Login[]>(this.baseUrl, this.httpOptions)
   }
 
   readById(id:number):Observable<Login>{
-    return this.http.get<Login>(this.baseUrl+"/"+id)
+    return this.http.get<Login>(this.baseUrl+"/"+id, this.httpOptions)
   }
 
 }
--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -1,5 +1,5 @@
 import { Stock } from './../model/stock.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { Observable } from 'rxjs';
@@ -12,9 +12,21 @@ export class StockService {
 
   baseUrl = environment.baseUrl+"stock"
 
+  httpOptions={};
+
   constructor(
               private snackBar: MatSnackBar,
-              private http: HttpClient) { }
+              private http: HttpClient) { 
+                
+                var headers_object = new HttpHeaders();
+                headers_object.append('Content-Type', 'application/json');
+                headers_object.append("Authorization", "Basic " + btoa("admin:password"));
+            
+                this.httpOptions = {
+                  headers: headers_object
+                };
+
+              }
 
   showMessage(msg:string):void{
     this.snackBar.open(msg,'x',
@@ -26,23 +38,23 @@ export class StockService {
   }
 
   create(stock:Stock): Observable<Stock>{
-    return this.http.post<Stock>(this.baseUrl, stock)
+    return this.http.post<Stock>(this.baseUrl, stock, this.httpOptions)
   }
 
   read():Observable<Stock[]>{
-    return this.http.get<Stock[]>(this.baseUrl)
+    return this.http.get<Stock[]>(this.baseUrl, this.httpOptions)
   }
 
   readById(id: number):Observable<Stock>{
-    return this.http.get<Stock>(this.baseUrl+"/"+id)
+    return this.http.get<Stock>(this.baseUrl+"/"+id, this.httpOptions)
   }
 
   delete(id:number):Observable<Stock[]>{
-    return this.http.delete<Stock[]>(this.baseUrl+"/"+id)
+    return this.http.delete<Stock[]>(this.baseUrl+"/"+id, this.httpOptions)
   }
 
   update(stock:Stock, id:number):Observable<Stock[]>{
-    return this.http.put<Stock[]>(this.baseUrl+"/"+id, stock)
+    return this.http.put<Stock[]>(this.baseUrl+"/"+id, stock, this.httpOptions)
   }
 
 }
